3c62c191504e7dc550186f5304d0d1afa00122f5
oni007 <o.ahmadi1992@gmail.com>
2022-04-13 15:49:34 +0430

    43 - Admin Lte

--- account/views.py
+++ account/views.py
@@ -4,10 +4,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from blog.models import Article
 
+
 @login_required
 def home(request):
     return render(request, 'registration/home.html')
-
 
 class ArticleList(LoginRequiredMixin, ListView):
     queryset = Article.objects.all()
