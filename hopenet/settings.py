4e230a6a135200394090c85649ab6e3593bcab6e
Omid Ahmadi <o.ahmadi1992@gmail.com>
2022-04-16 19:19:42 +0430

    54 - Log Out View

--- hopenet/settings.py
+++ hopenet/settings.py
@@ -16,6 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL = "account:home"
 LOGIN_URL = "account:login"
+LOGOUT_REDIRECT_URL = "account:login"
 
 
 # Quick-start development settings - unsuitable for production
