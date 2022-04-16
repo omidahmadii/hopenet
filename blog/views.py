274710244bf8d9a243b3b5ec590fbdf99f8f4851
oni007 <o.ahmadi1992@gmail.com>
2022-04-16 15:57:51 +0430

    52

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.models import User
+from account.models import User
 from .models import Article, Category
 from django.views.generic import ListView, DetailView
 
