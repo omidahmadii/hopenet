cbe714dac02e21995938afb8a67da5ed7a0e5d44
Omid Ahmadi <o.ahmadi1992@gmail.com>
2022-04-16 17:32:30 +0430

    53 - Delete View

--- account/mixins.py
+++ account/mixins.py
@@ -38,3 +38,11 @@ class AuthorAccessMixin():
 			return super().dispatch(request, *args, **kwargs)
 		else:
 			raise Http404("You Are not Permitted To Update this Article ")
+
+
+class SuperUserAccessMixin():
+	def dispatch(self, request, *args, **kwargs):
+		if request.user.is_superuser:
+			return super().dispatch(request, *args, **kwargs)
+		else:
+			raise Http404("You Are not SuperUser")
--- account/views.py
+++ account/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .mixins import FieldsMixin, FormValidMixin, AuthorAccessMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from .mixins import (
+    FieldsMixin, FormValidMixin, AuthorAccessMixin, SuperUserAccessMixin
+)
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView
+)
 from blog.models import Article
+from django.urls import reverse_lazy
+
 
 
 class ArticleList(LoginRequiredMixin, ListView):
@@ -24,3 +30,9 @@ class ArticleCreate(LoginRequiredMixin, FormValidMixin, FieldsMixin, CreateView)
 class ArticleUpdate(AuthorAccessMixin, FormValidMixin, FieldsMixin, UpdateView):
     model = Article
     template_name = "registration/article-create-update.html"
+
+
+class ArticleDelete(SuperUserAccessMixin, DeleteView):
+    model = Article
+    success_url = reverse_lazy('account:home')
+    template_name = "registration/article_confirm_delete.html"
